65b2adf6971cb3c87bec0f9e8c1f540594052789
Oleksandr Prokopenko <Oleksandr.Prokopenko92@gmail.com>
2023-10-14 10:04:08 +0300

    Starting to create an exit from the personal account

--- Django_blog/urls.py
+++ Django_blog/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views as usersViews
+from django.contrib.auth import views as authViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # При переході на головну сторінку піключаємо файл (blog.urls.py)
     path('', include('blog.urls')),
     # Сторінка регістрації (users.views.register)
-    path('registration', usersViews.register, name='registration')
+    path('registration/', usersViews.register, name='registration'),
+    # Використовуємо вбудовані класси django
+    path('user/',authViews.LoginView.as_view() , name='user'),
+    path('user/',authViews.LogoutView.as_view() , name='logout'),
 ]
