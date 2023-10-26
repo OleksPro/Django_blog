2262c9972b1da818c5c796bed0c135ca503c9094
Oleksandr Prokopenko <Oleksandr.Prokopenko92@gmail.com>
2023-10-26 14:00:09 +0300

    Page output of posts on the main page

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     # до файла blog.views.py i звертаємось до метода home
     path('', views.ShowNewsView.as_view(), name='home'),
     # Відстежує динамічну сторінку
+    path('user/<str:username>', views.UserAllNewsView.as_view(), name='user-news'),
     path('news/<int:pk>', views.NewsDetailView.as_view(), name='news-detail'),
     path('news/<int:pk>/update', views.UpdateNewsView.as_view(), name='news-update'),
     path('news/<int:pk>/delete', views.DeleteNewsView.as_view(), name='news-delete'),
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from .models import News
 from django.views.generic import (
     ListView, 
@@ -14,13 +15,33 @@ class ShowNewsView(ListView):
     model = News
     template_name = 'blog/home.html'
     context_object_name = 'news'
-    ordering = ['date']
+    # Сортування статей по даті
+    ordering = ['-date']
+    # Додає пагінацію
+    paginate_by = 5
 
-    def get_context_data(self, **kwards):
-        context = super(ShowNewsView, self).get_context_data(**kwards)
+    def get_context_data(self, **kwargs):
+        context = super(ShowNewsView, self).get_context_data(**kwargs)
         context['title'] = 'Головна сторінка'
         return context
 
+# Перехід на сторінку зі статтями від одного автора
+class UserAllNewsView(ListView):
+    model = News
+    template_name = 'blog/user_news.html'
+    context_object_name = 'news'
+    # Додає пагінацію
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return News.objects.filter(avtor=user).order_by('-date')
+
+    def get_context_data(self, **kwards):
+        context = super(UserAllNewsView, self).get_context_data(**kwards)
+        context['title'] = f"Cтатті від користувача {self.kwargs.get('username')}"
+        return context
+    
 # Перехід на статтю
 class NewsDetailView(DetailView):
     model = News
