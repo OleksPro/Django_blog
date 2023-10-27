297d09721bd6c6d65e229849f551f73c639ddbf1
Oleksandr Prokopenko <Oleksandr.Prokopenko92@gmail.com>
2023-10-27 20:03:18 +0300

    Add Password recovery

--- Django_blog/urls.py
+++ Django_blog/urls.py
@@ -7,14 +7,34 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     # При переході на головну сторінку піключаємо файл (blog.urls.py)
     path('', include('blog.urls')),
+
     # Сторінка регістрації (users.views.register)
     path('registration/', usersViews.register, name='registration'),
     path('profile/', usersViews.profile, name='profile'),
+
     # Використовуємо вбудовані класси django
     path('user/',authViews.LoginView.as_view(template_name='users/user.html') , name='user'),
-    path('exit/',authViews.LogoutView.as_view(template_name='users/exit.html') , name='exit'), 
+    path('exit/',authViews.LogoutView.as_view(template_name='users/exit.html') , name='exit'),
+
+    # Відновлення паролю
+    path('password_reset_complite/',authViews.PasswordResetConfirmView.as_view(
+        template_name='users/password_reset_complite.html'), 
+        name='password_reset_complite'), 
+
+    path('password_reset/',authViews.PasswordResetView.as_view(
+        template_name='users/password_reset.html'), 
+        name='password_reset'), 
+
+    path('password_reset_confirm/<uidb64>/<token>/', 
+        authViews.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+        name='password_reset_confirm'),
+
+    path('password_reset_done/', 
+        authViews.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
+        name='password_reset_done')
 ]
 
 if settings.DEBUG:
